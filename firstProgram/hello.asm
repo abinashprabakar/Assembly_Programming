62ffeab072e6fc37e9f0a6ef4d8e4cc757ad136e
abinashprabakar <abinashprabakaran@gmail.com>
2021-09-27 12:55:15 +0530

    First Program

--- firstProgram/hello.asm
+++ firstProgram/hello.asm
@@ -1,17 +1,17 @@
-section .data 
-	message db "Hello",10	;print message Hello  10 or 0x0a => newline
-	len equ $ - message	;length of the message
+section .data
+    msg db "Hello world!",10    ; 10 is the ASCII code for a new line (LF)
 
 section .text			;section declaration
-	global _start		;for exporting symbols in code to where it points in the object code generated
+    global _start		;for exporting symbols in code to where it points in the object code generated
 
 _start:
-	mov rax,1		;store 1 to register rax because to make a call to the operating system
-	mov rdi,1		;stores 1 to rdi, where 1 stands for STDOUT (write to console)
-	mov rsi,message		;message is stored in rsi, rsi on a 64bit processor only have 64 bit each
-	mov rdx,14		;setting the value of rdx to the number of bytes/character to print
-	int 0x80		;to invoke a system call
-
-	mov rax,60		;system call for exit
-	mov rdi,0
-	syscall			;default way of entering kernel mode on x86-64
+    mov rax, 1			;store value 1 to rax to make a call to the operating system
+    mov rdi, 1			;store value 1 to rdi, where 1 stands for STDOUT (write to console)
+    mov rsi, msg		;msg string is stored in rsi, rsi on 64 bit processor only have 64bit each 
+    mov rdx, 13			;setting the value of rdx to the number of bytes/character to print
+    syscall			;default way of entering kernel mode on x86_64
+    				;int 0x80 is other way of entering kernel mode
+   
+    mov rax, 60			;system call for exit
+    mov rdi, 0
+    syscall
